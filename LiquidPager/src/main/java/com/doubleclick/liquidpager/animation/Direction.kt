4887dbb28786de24c783f499e8414253352a6693
eslam <eslamghazy600@gmail.com>
2023-07-14 00:37:50 +0300

    Liquid Pager

---
+++ LiquidPager/src/main/java/com/doubleclick/liquidpager/animation/Direction.kt
@@ -0,0 +1,7 @@
+package com.doubleclick.liquidpager.animation
+
+object Direction {
+    val NONE = -1
+    val LEFT = 0
+    val RIGHT = 1
+}
---
+++ app/src/main/java/com/doubleclick/liquidpager/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.doubleclick.liquidpager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val pager = findViewById<LiquidPager>(R.id.pager)
+        pager.adapter = Adapter(supportFragmentManager)
+    }
+}
